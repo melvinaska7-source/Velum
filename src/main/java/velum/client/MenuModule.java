34195684a5d63998a54317795e878857a1ce5409
melvinaska7-source <_email@example.com>
2026-09-20 08:18:47 +0300

    Wexside

--- src/main/java/velum/client/MenuModule.java
+++ src/main/java/velum/client/MenuModule.java
@@ -25,6 +25,7 @@ extends Module {
     private ModeSetting I_field_bbe33e6c;
     private ModeSetting.Nested1_42765c60 I_field_500d0627;
     private ModeSetting.Nested1_42765c60 i_field_500d0627;
+    private ModeSetting.Nested1_42765c60 I_field_500d0628;
     private KeybindSetting I_field_ba20522c;
     private Screen I_field_bf52cf84;
     private IiiIiIiii_Class216 I_field_a5e7424c;
@@ -39,12 +40,16 @@ extends Module {
         this.I_field_bbe33e6c = new ModeSetting(this, "modules.settings.menu.mode");
         this.I_field_500d0627 = new ModeSetting.Nested1_42765c60(this.I_field_bbe33e6c, "modules.settings.menu.mode.dropdown");
         this.i_field_500d0627 = new ModeSetting.Nested1_42765c60(this.I_field_bbe33e6c, "modules.settings.menu.mode.modern").select();
+        this.I_field_500d0628 = new ModeSetting.Nested1_42765c60(this.I_field_bbe33e6c, "modules.settings.menu.mode.premium");
         this.I_field_ba20522c = new KeybindSetting(this, "modules.settings.menu.hide_key").I_method_4288e15a(342);
     }
 
     @Override
     public void onEnable() {
         boolean bl = this.i_field_500d0627.isSelected();
+        if (this.I_field_bbe33e6c.I_method_1544ce2d("modules.settings.menu.mode.premium") && MenuModule.I_field_3a9bda27.currentScreen instanceof PremiumMenuScreen) {
+            return;
+        }
         if (bl && MenuModule.I_field_3a9bda27.currentScreen instanceof IiiIiIiiI_Class215) {
             return;
         }
@@ -62,6 +67,9 @@ extends Module {
     }
 
     public Screen I_method_83034dbb() {
+        if (this.I_field_bbe33e6c.I_method_1544ce2d("modules.settings.menu.mode.premium")) {
+            return new PremiumMenuScreen();
+        }
         if (this.i_field_500d0627.isSelected()) {
             IiiIIiiiI_Class207 iiiIIiiiI_Class207 = VelumClient.getInstance().I_method_96982062();
             IiiIIiiiI_Class207 iiiIIiiiI_Class2072 = iiiIIiiiI_Class207 instanceof IiiIiIiiI_Class215 ? iiiIIiiiI_Class207 : new IiiIiIiiI_Class215();
